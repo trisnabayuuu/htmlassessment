49664135ec4bacf036d95992cbaab920bd904dc5
Trisna Bayu <btrisna90@gmail.com>
2023-09-19 09:38:50 +0700

    routing

---
+++ js/route.js
@@ -0,0 +1,12 @@
+function register() {
+    window.location.href = "register.html";
+}
+function homePage() {
+    window.location.href = "landing-page.html";
+}
+function login() {
+    window.location.href = "login.html";
+}
+function detail(){
+    window.location.href = "detail.html"
+}
